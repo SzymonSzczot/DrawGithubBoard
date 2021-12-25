6ab712a5aec921e4db50d94db57788b6c2a8318b
SzymonSzczotKivy <szymon.szczot@kivy.pl>
2021-12-25 18:06:52 +0100

    abcd model

--- ml_app/constants.py
+++ ml_app/constants.py
@@ -1,8 +1,8 @@
-PATH = "models/a_b_c_model.pth"
+PATH = "models/a_b_c_d_model.pth"
 
 input_size = 35
 hidden_size = 700
-num_classes = 3
+num_classes = 4
 num_epochs = 40
 batch_size = 5
 learning_rate = 0.00001
